3bb398a0c24d935a981ddefb14bad7f5d4d91e33
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2019-10-15 19:52:49 +0300

    Завершил задание

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
-  entry: `./src/index.js`,
+  entry: `./src/index.jsx`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
@@ -23,5 +23,8 @@ module.exports = {
       }
     ],
   },
-  devtool: `source-map`
+  devtool: `source-map`,
+  resolve: {
+    extensions: [`.js`, `.jsx`]
+  }
 };
