ae030710f14b4580a7171b229d09ad72389aac14
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2019-10-19 12:17:52 +0300

    Завершил Задание

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import {MainPage} from "../main-page/main-page";
 
+const filmsName = [
+  `Fantastic Beasts`,
+  `Bohemian Rhapsody`,
+  `Macbeth`
+];
+
 export const App = () => {
-  return <MainPage />;
+  return <MainPage filmsName={filmsName} />;
 };
